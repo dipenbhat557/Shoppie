73b5dc1d5fefdbb42596911463ea4096c99b2be8
Avashneupane9857 <juisch20220011252@gmail.com>
2024-08-25 15:48:33 +0530

    Check commit

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -6,7 +6,7 @@ import CategorywiseProduct from "./components/CategorywiseProduct";
 
 export default async function Home() {
   const products = await fetchProducts();
-  const categories = [...new Set(products.map(p => p.category))];
+  const categories = [...new Set(products.map((p) => p.category))];
 
   return (
     <div>
@@ -14,16 +14,18 @@ export default async function Home() {
 
       <div className="container mx-auto px-4 border border-grey-800 mt-2 p-2 rounded-lg">
         <h1 className="text-2xl sm:text-4xl font-bold my-8 text-grey-800">
-          Featured Products
+          Featured Product
         </h1>
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
-          {products.slice(0, 4).map(product => (
+          {products.slice(0, 4).map((product) => (
             <ProductItem key={product.id} product={product} />
           ))}
         </div>
         <div className="text-center mt-8">
           <Link href="/products">
-            <p className="text-grey-800 hover:underline text-right">View All Products</p>
+            <p className="text-grey-800 hover:underline text-right">
+              View All Products
+            </p>
           </Link>
         </div>
       </div>
