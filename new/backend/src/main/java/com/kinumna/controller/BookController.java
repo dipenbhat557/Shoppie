49b07a5b4baaf0b861b13967f4ee112244775df3
dipenbhat557 <dipenbhat557@gmail.com>
2024-08-31 02:21:56 +0530

    :sparkles: configured application properties

---
+++ new/backend/src/main/java/com/kinumna/controller/BookController.java
@@ -0,0 +1,22 @@
+package com.kinumna.controller;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import com.kinumna.model.Author;
+import com.kinumna.model.Book;
+
+@Controller
+public class BookController {
+    @QueryMapping
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.authorId());
+    }
+}
