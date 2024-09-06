96091161fe19028472abba349105070e34d4f2fa
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-06 11:53:11 +0530

    :sparkles: updated user and address controller

--- new/backend/src/main/java/com/kinumna/controller/AddressController.java
+++ new/backend/src/main/java/com/kinumna/controller/AddressController.java
@@ -3,39 +3,50 @@ package com.kinumna.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.kinumna.model.Address;
 import com.kinumna.payload.requests.AddressInput;
 import com.kinumna.service.AddressService;
 
-@Controller
+@RestController
+@RequestMapping("/api/v1/address")
 public class AddressController {
     
     @Autowired
     private AddressService addressService;
 
-    public Address getAddress( Integer id) {
-        return this.addressService.findById(id);
+    @GetMapping("/{id}")
+    public ResponseEntity<Address> getAddress(@PathVariable Integer id) {
+        return new ResponseEntity<>( this.addressService.findById(id),HttpStatus.OK);
     }
 
-    
-    public List<Address> listAddresses() {
-        return addressService.findAll();
+    @GetMapping
+    public ResponseEntity<List<Address>> listAddresses() {
+        return new ResponseEntity<>( addressService.findAll(),HttpStatus.OK);
     }
 
-    
-    public Address createAddress( AddressInput input) {
-        return addressService.save(input);
+    @PostMapping
+    public ResponseEntity<Address> createAddress(@RequestBody AddressInput input) {
+        return new ResponseEntity<>( addressService.save(input),HttpStatus.OK);
     }
 
-    
-    public Address updateAddress( Integer id,  AddressInput input) {
-        return this.addressService.updateAddress(id, input);
+    @PutMapping("/{id}")
+    public ResponseEntity< Address> updateAddress(@PathVariable Integer id, @RequestBody  AddressInput input) {
+        return ResponseEntity.ok(this.addressService.updateAddress(id, input)) ;
     }
 
-    
-    public Boolean deleteAddress( Integer id) {
-        return this.addressService.deleteById(id);
+    @DeleteMapping("/{id}")
+    public ResponseEntity< Boolean> deleteAddress(@PathVariable Integer id) {
+        return ResponseEntity.ok( this.addressService.deleteById(id));
     }
 }
