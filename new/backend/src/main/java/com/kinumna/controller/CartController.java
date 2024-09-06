5e3739035ed8bb3d111b909d16d23fb03bed3772
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-06 22:29:23 +0530

    :sparkles: added cart response and service

--- new/backend/src/main/java/com/kinumna/controller/CartController.java
+++ new/backend/src/main/java/com/kinumna/controller/CartController.java
@@ -1,8 +1,17 @@
 package com.kinumna.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.kinumna.service.CartService;
+
+@RestController
+@RequestMapping("/api/v1/cart")
 public class CartController {
+    
+    @Autowired
+    private CartService cartService;
+
     
 }
--- new/backend/src/main/java/com/kinumna/serviceImpl/CartServiceImpl.java
+++ new/backend/src/main/java/com/kinumna/serviceImpl/CartServiceImpl.java
@@ -1,10 +1,48 @@
 package com.kinumna.serviceImpl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.kinumna.payload.responses.CartResponse;
+import com.kinumna.repo.CartRepo;
 import com.kinumna.service.CartService;
 
 @Service
 public class CartServiceImpl implements CartService{
+
+    @Autowired
+    private CartRepo cartRepo;
+
+    @Override
+    public CartResponse create(int userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'create'");
+    }
+
+    @Override
+    public CartResponse getById(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getById'");
+    }
+
+    @Override
+    public CartResponse update(int id, int userId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'update'");
+    }
+
+    @Override
+    public List<CartResponse> getAll() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAll'");
+    }
+
+    @Override
+    public void delete(int id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'delete'");
+    }
     
 }
