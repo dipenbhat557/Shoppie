57f5ed249967f39ab33d8e8da7a244284dc4131e
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-09 18:58:36 +0530

    :sparkles: added review service

--- new/backend/src/main/java/com/kinumna/service/ReviewService.java
+++ new/backend/src/main/java/com/kinumna/service/ReviewService.java
@@ -1,5 +1,16 @@
 package com.kinumna.service;
 
+import java.util.List;
+
+import com.kinumna.payload.requests.ReviewInput;
+import com.kinumna.payload.responses.ReviewResponse;
+
 public interface ReviewService {
-    
+    public ReviewResponse create(ReviewInput input);
+    public List<ReviewResponse> getAll();
+    public ReviewResponse getById(int id);
+    public List<ReviewResponse> getByUser(int userId);
+    public List<ReviewResponse> getByProduct(int productId);
+    public ReviewResponse update(int id, ReviewInput input);
+    public void delete(int id);    
 }
