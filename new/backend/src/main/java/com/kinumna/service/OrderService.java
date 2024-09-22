c112a2a4e6aa9c2c8c7a417d781faf4111d95277
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-22 22:18:56 +0530

    :sparkles: added order service

--- new/backend/src/main/java/com/kinumna/service/OrderService.java
+++ new/backend/src/main/java/com/kinumna/service/OrderService.java
@@ -1,8 +1,13 @@
 package com.kinumna.service;
 
+import java.util.List;
+
 import com.kinumna.payload.requests.OrderInput;
 import com.kinumna.payload.responses.OrderResponse;
 
 public interface OrderService {
-   public OrderResponse createOrder(OrderInput request); 
+   public OrderResponse createOrder(OrderInput request);
+   public OrderResponse getByUser(int userId);
+   public List<OrderResponse> getAll();
+   public OrderResponse getById(int orderId); 
 }
