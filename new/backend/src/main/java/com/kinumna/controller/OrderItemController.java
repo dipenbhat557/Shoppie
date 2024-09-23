978345964cbbfd6ae861461bc0603091db277ecf
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-23 23:17:48 +0530

    :sparkles: added order item controller structure

--- new/backend/src/main/java/com/kinumna/controller/OrderItemController.java
+++ new/backend/src/main/java/com/kinumna/controller/OrderItemController.java
@@ -1,8 +1,23 @@
 package com.kinumna.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import com.kinumna.model.OrderItem;
+import com.kinumna.service.OrderItemService;
+
+@RestController
+@RequestMapping("/api/v1/order-item")
 public class OrderItemController {
     
+    @Autowired
+    private OrderItemService orderItemService;
+
+    @PostMapping
+    public ResponseEntity<OrderItem> create(){
+        return null;
+    }
 }
