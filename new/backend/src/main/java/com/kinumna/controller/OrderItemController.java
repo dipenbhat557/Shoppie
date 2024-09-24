536ce31a99dcddbe87bd6402ae4e2a318d499a15
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-24 12:24:24 +0530

    :sparkles: added endpoints in order item controller

--- new/backend/src/main/java/com/kinumna/controller/OrderItemController.java
+++ new/backend/src/main/java/com/kinumna/controller/OrderItemController.java
@@ -1,8 +1,14 @@
 package com.kinumna.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +24,26 @@ public class OrderItemController {
 
     @PostMapping
     public ResponseEntity<OrderItem> create(){
+        return null;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<OrderItem>> getAll(){
+        return null;
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderItem> getById(@PathVariable int id){
+        return null;
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<OrderItem> update(@PathVariable int id){
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteById(@PathVariable int id){
         return null;
     }
 }
