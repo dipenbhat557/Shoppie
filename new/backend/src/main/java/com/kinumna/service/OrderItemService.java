5ef251166e9f46ac2ca12ce808f99e885c768d03
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-27 23:08:34 +0530

    :sparkles: added order item service

--- new/backend/src/main/java/com/kinumna/service/OrderItemService.java
+++ new/backend/src/main/java/com/kinumna/service/OrderItemService.java
@@ -1,5 +1,14 @@
 package com.kinumna.service;
 
+import java.util.List;
+
+import com.kinumna.payload.requests.OrderItemInput;
+import com.kinumna.payload.responses.OrderItemResponse;
+
 public interface OrderItemService {
-    
+    public OrderItemResponse create(OrderItemInput input);
+    public OrderItemResponse getById(int orderItemId);
+    public List<OrderItemResponse> getAll();
+    public OrderItemResponse update(int id, OrderItemInput input);
+    public void delete(int id);
 }
