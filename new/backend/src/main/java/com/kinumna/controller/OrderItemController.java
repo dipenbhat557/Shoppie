aeb5407cfb3e051c9daae27947d4b8bfa44f35ba
dipenbhat557 <dipenbhat557@gmail.com>
2024-09-30 23:34:33 +0530

    :sparkles: added endpoints for order item

--- new/backend/src/main/java/com/kinumna/controller/OrderItemController.java
+++ new/backend/src/main/java/com/kinumna/controller/OrderItemController.java
@@ -3,16 +3,20 @@ package com.kinumna.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kinumna.model.OrderItem;
+import com.kinumna.payload.requests.OrderItemInput;
+import com.kinumna.payload.responses.OrderItemResponse;
 import com.kinumna.service.OrderItemService;
 
 @RestController
@@ -23,27 +27,28 @@ public class OrderItemController {
     private OrderItemService orderItemService;
 
     @PostMapping
-    public ResponseEntity<OrderItem> create(){
-        return null;
+    public ResponseEntity<OrderItemResponse> create(@RequestBody OrderItemInput input){
+        return new ResponseEntity<>(this.orderItemService.create(input),HttpStatus.OK);
     }
 
     @GetMapping
-    public ResponseEntity<List<OrderItem>> getAll(){
-        return null;
+    public ResponseEntity<List<OrderItemResponse>> getAll(){
+        return ResponseEntity.ok(this.orderItemService.getAll());
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<OrderItem> getById(@PathVariable int id){
-        return null;
+    public ResponseEntity<OrderItemResponse> getById(@PathVariable int id){
+        return ResponseEntity.ok(this.orderItemService.getById(id));
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<OrderItem> update(@PathVariable int id){
-        return null;
+    public ResponseEntity<OrderItemResponse> update(@PathVariable int id, @RequestBody OrderItemInput input){
+        return ResponseEntity.ok(this.orderItemService.update(id, input));
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteById(@PathVariable int id){
-        return null;
+        this.orderItemService.delete(id);
+        return ResponseEntity.ok("Deleted Successfully!!");
     }
 }
