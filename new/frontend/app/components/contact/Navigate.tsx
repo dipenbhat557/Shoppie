37e0811787dc6d768459baad33ec367c06a4369e
Thinley <thinleylama44@gmail.com>
2024-10-15 12:51:48 +0530

    FEAT: Add new contact page component

---
+++ new/frontend/app/components/contact/Navigate.tsx
@@ -0,0 +1,28 @@
+import { Slash } from "lucide-react"
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb"
+
+export function Navigate() {
+  return (
+    <Breadcrumb>
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink href="/">Home</BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator>
+          <Slash />
+        </BreadcrumbSeparator>
+        <BreadcrumbItem>
+          <BreadcrumbPage>Contact</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </Breadcrumb>
+  )
+}
--- new/frontend/app/page.tsx
+++ new/frontend/app/page.tsx
@@ -13,21 +13,33 @@ import service from "../public/images/service.png";
 import deliver from "../public/images/delivery.png";
 import support from "../public/images/support.png";
 import SupportBar from "./components/Support";
+import { Topbar } from "./components/Topbar";
+import Navbar from "./components/Navbar";
+import { CategoryNav } from "./components/CategoryNav";
+import { Footer } from "./components/Footer";
 
 export default function Home() {
   return (
-    <div>
-      <CarouselComponent />
-      <TopDeals />
-      <MobileAddSection />
-      <BestDeals />
-      <HomeDecor />
-      <HomeDecorMobileComponent />
-      <DressStyleMobileComponent />
-      <RecentItem />
-      <AdBanner1 />
-      <BuyAgainComp />
-      <SupportBar service={service} deliver={deliver} support={support} />
-    </div>
+    <>
+      <div className="flex flex-col min-h-screen  w-full  justify-between items-center ">
+        <div className="w-full flex flex-col gap-5  ">
+          <Topbar />
+          <Navbar />
+          <CategoryNav />
+        </div>
+        <CarouselComponent />
+        <TopDeals />
+        <MobileAddSection />
+        <BestDeals />
+        <HomeDecor />
+        <HomeDecorMobileComponent />
+        <DressStyleMobileComponent />
+        <RecentItem />
+        <AdBanner1 />
+        <BuyAgainComp />
+        <SupportBar service={service} deliver={deliver} support={support} />
+        <Footer />
+      </div>
+    </>
   );
 }
