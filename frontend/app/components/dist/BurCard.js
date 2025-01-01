f5cadba0a0c3d205d11185cdb2a7fa0d2c3a9f39
Thinley <thinleylama44@gmail.com>
2025-01-02 00:13:06 +0530

    Remove dist folder

--- frontend/app/components/dist/BurCard.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.BurCard = void 0;
-exports.BurCard = function () {
-    return (React.createElement("div", null, "BurCard"));
-};
--- frontend/app/components/dist/MobileAddSection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.MobileAddSection = void 0;
-var BestDealsAdd_1 = require("./BestDealsAdd");
-exports.MobileAddSection = function () {
-    return (React.createElement("div", { className: " flex felx-col  sm:hidden     max-w-screen-sm" },
-        React.createElement(BestDealsAdd_1.BestDealsAdd, null)));
-};
